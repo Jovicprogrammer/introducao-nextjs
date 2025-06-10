7a4f158c0e6fb8813a0b846303241364442ce363
Jovic_Penhaz <victosousa92@gmail.com>
2025-06-10 09:21:28 -0300

    finalizando a página dogs

--- src/app/dogs/page.tsx
+++ src/app/dogs/page.tsx
@@ -11,34 +11,38 @@ type Dog = {
 export default function PostPage() {
   const [dog, setDog] = useState<Dog | null>(null)
 
-  useEffect(() => {
-    const fetchDog = async () => {
-      const res = await fetch("https://dog.ceo/api/breeds/image/random")
-      const data = await res.json()
-      setDog(data)
+  const fetchDog = async () => {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random")
+    const data = await res.json()
+    setDog(data)
+  }
 
-    }
+  useEffect(() => {
     fetchDog()
   }, [])
 
   return (
-    <div className="bg-violet-400 min-h-screen p-6">
-      <h1 className="text-3xl font-bold mb-6">Post do Dia</h1>
+    <div className="bg-violet-400 min-h-screen p-6 flex flex-col items-center ">
+      <h1 className="text-3xl font-bold mb-6 text-center">Um Cão Aleatório 🐶</h1>
 
       {dog ? (
-        <div className="bg-indigo-400 shadow-lg p-4 m-4 rounded">
-          <Image src={dog.message}
-                    alt="nextjs logo "
-                    height={400}
-                    width={400}
-                    className=" "
-                    />
+        <div className="bg-indigo-400 shadow-lg p-4 m-4 rounded justify-center items-center">
+          <Image 
+            src={dog.message}
+            alt="Cachorro aleatório"
+            height={250}
+            width={250}
+            className="justify-items-center"
+          />
         </div>
       ) : (
         <p>Carregando post...</p>
       )}
+
+      <div>
+        <button className="bg-indigo-500 text-white py-2 px-3 hover:bg-indigo-400 rounded cursor-pointer transition" onClick={fetchDog}>Novo cão</button>
+      </div>
     </div>
+    
   )
-
-
 }
