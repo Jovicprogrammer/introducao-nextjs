275577ac995839b24de4574fea29d013f46f83a6
Jovic_Penhaz <victosousa92@gmail.com>
2025-08-04 20:06:08 -0300

    feat: novos cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,11 +9,11 @@ export default function Card({title, description, imageUrl}: CardProps) {
 
     return (
 
-        <div className="bg-blue-950 max-w-sm flex items-center flex-col m-2 rounded-2xl py-5 px-4 space-y-2 border-4 border-amber-200 ">
+        <div className="bg-blue-950 max-w-sm flex items-center flex-col m-2 rounded-2xl py-5 px-5 space-y-3 border-4 border-amber-200 ">
             
             <h2 className="text-amber-200 ">{title}</h2>
 
-            <Image className="border-3 border-amber-200 rounded hover:opacity-75 transition"
+            <Image className="border-3 border-amber-200 rounded hover:opacity-75 transition cursor-pointer"
             src={imageUrl}
             width={200}
             height={200}
