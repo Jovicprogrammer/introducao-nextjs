a12654302ce48b472a685cf995deaf7c911313bf
Jovic_Penhaz <victosousa92@gmail.com>
2025-08-05 12:27:21 -0300

    categorias + cards com map

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,9 +6,40 @@ import Card from "@/components/Card"
 
 export default function About() {
 
-    return (
-
+     const items = [
+        { title: 'QUADECA',
+            description: 'Vanished from the real world, Quad take a journey through a destroyed world to find the meaning of life.',
+            imageUrl: '/quadeca.png',
+            category: 'Humano'},
         
+        { title: 'THE GREAT BAKUNAWA',
+            description: 'The legendary monster serpent that eats the moon and brings the eclipse. Look out.', imageUrl: '/bakunawa.png',
+            category: 'Monstro'},
+
+        { title: 'DEATH',
+            description: 'With their mysterious boat and funeral grim, DEATH comes with your time.',
+            imageUrl: '/barqueiro.png',
+            category: 'Monstro'},
+
+        { title: 'THE SHIP',
+            description: 'Discover new worlds with this grand construction.',
+            imageUrl: '/ship.png',
+            category: 'Objeto'},
+
+        { title: 'LIGHTHOUSE',
+            description: 'The light the sailors need to not get lost.',
+            imageUrl: '/lighthouse.png',
+            category: 'Lugar'},
+
+        { title: 'THE OCEAN',
+            description: 'The enourmous body of water that hides all kinds of danger',
+            imageUrl: '/ocean.png',
+            category: 'Lugar'},
+
+
+      ];
+
+    return (
 
         <div>
 
@@ -29,18 +60,22 @@ export default function About() {
                 <Button label="THE GREAT BAKUNAWA"></Button>
                 <Button label="WAGING WAR"></Button>
                 <Button label="DANCING WITHOUT MOVING"></Button>
-                <Button label="I DREAM ABOUT SINKING"></Button>
+                <Button label="FORGONE"></Button>
                 <Button label="NO QUESTIONS ASKED"></Button>
                 <Button label="NATURAL CAUSES"></Button>
+                <Button label="CASPER"></Button>
             </div>
-<div className="flex">
+<div className="flex flex-wrap justify-center ">
     
-                <Card title="QUADECA" description="Vanished from the real world, Quad take a journey through a destroyed world to find the meaning of life." imageUrl="/quadeca.png"></Card>
-    
-                <Card title="THE GREAT BAKUNAWA" description="The legendary monster serpent that eats the moon and brings the eclipse. Look out." imageUrl="/bakunawa.png"></Card>
+         {items.map((item) => (
+            <Card key={item.title} title={item.title} description={item.description} imageUrl={item.imageUrl} category={item.category}></Card>
+          ))}
 
-                <Card title="DEATH" description="With their mysterious boat and funeral grim, DEATH comes with your time." imageUrl="/barqueiro.png"></Card>
 </div>
+
+            
+           
+            
 
         </div>
         </div>
