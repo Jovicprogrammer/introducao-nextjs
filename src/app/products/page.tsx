1c95a0749165a1da6019e7cf531877c1fa00cb78
Jovic_Penhaz <victosousa92@gmail.com>
2025-08-22 16:21:54 -0300

    feat: botão de reprodução

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,4 @@
+import BotaoReproducao from "@/components/BotaoReproducao";
 import Navbar from "@/components/Navbar";
 
 export default function Products() {
@@ -17,21 +18,8 @@ export default function Products() {
             }/>
             </div>
 
-        <h1>Nossos produtos</h1>
-        
         <div>
-            <h2>Balões de festa na cor rosa - 1000 unidades</h2>
-            <h3>R$9,50</h3>
-        </div>
-
-        <div>
-            <h2>Bolo de aniversário clássico (sob encomenda)</h2>
-            <h3>R$50,00</h3>
-        </div>
-
-        <div>
-            <h2>Caixa de música festiva</h2>
-            <h3>R$80,00</h3>
+            <BotaoReproducao />
         </div>
 
         </div>
